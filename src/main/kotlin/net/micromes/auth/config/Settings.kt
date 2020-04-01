29448213e40392c76c0851b65027653ac81b5afb
Universumgames <tommyios.ta@gmail.com>
2020-04-01 23:35:54 +0200

    reorganized db settings

--- src/main/kotlin/net/micromes/auth/config/Settings.kt
+++ src/main/kotlin/net/micromes/auth/config/Settings.kt
@@ -7,5 +7,7 @@ class Settings {
     companion object {
         const val CLIENT_ID = "1025113353398-pb40di8kma99osibf68j8ov8fqvddr96.apps.googleusercontent.com"
         const val CLIENT_SECRET = "ZtS_4ANT1xX3SPlNgPIMjNzW"
+        const val DB_USER = "micromes.auth"
+        const val DB_PASSWORD = "MicromesAuth"
     }
 }
--- src/main/kotlin/net/micromes/auth/db/DB.kt
+++ src/main/kotlin/net/micromes/auth/db/DB.kt
@@ -1,10 +1,11 @@
 package net.micromes.auth.db
 
+import net.micromes.auth.config.Settings
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun dBConnect() {
-    Database.connect(url="jdbc:mysql://localhost:3306/micromes-auth?autoReconnect=false&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Berlin&allowPublicKeyRetrieval=true", driver="com.mysql.cj.jdbc.Driver", user = "micromes.auth", password = "MicromesAuth")
+    Database.connect(url="jdbc:mysql://localhost:3306/micromes-auth?autoReconnect=false&useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Berlin&allowPublicKeyRetrieval=true", driver="com.mysql.cj.jdbc.Driver", user = Settings.DB_USER, password = Settings.DB_PASSWORD)
 }
 
 fun dBInit() {
